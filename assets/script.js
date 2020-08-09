400c872c34a397eb11264cf663b3823e3c54ab4a
dfabert <glen.fabert@gmail.com>
2020-08-09 17:15:19 -0400

    placed time and date at the top of the page

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,16 @@
-function updateTimes() {
+$(document).ready(function () {
+    $('#currentDate').text(moment().format('MMMM DD YYYY'));
+    $('#currentTime').text(moment().format('h:mm a'));
+})
+
+
+function updateStyles() {
     var currentDate = new Date();
     console.log(currentDate);
 
     $('.hour').each(function() {
-         
         var thisHour  = ($(this).data('hour'));
-
         var diff = currentDate.getHours()- thisHour;
-        console.log(diff);
 
         if(diff < 0) {
             $(this).find('textarea').addClass('future');
@@ -19,4 +22,18 @@ function updateTimes() {
     })
 }
 
-updateTimes();
+function pullData() {
+    console.log('Pulling data from local storage');
+
+}
+
+$('.save-button').on('click', function(){
+    var textValue = $(this).siblings('textarea').val();
+    var parentHour = $(this).closest('hour').data('hour');
+
+    localStorage.setItem(parentHour,textValue);
+})
+
+
+updateStyles();
+pullData();
